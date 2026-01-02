07ae398c9f73b1ff1b8c670dbc2ffef2e931a4c2
prchalPetr <peta.prchal@email.cz>
2026-01-02 16:28:24 +0100

    create simple main page

---
+++ src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MainTemplateComponent } from "../../templates/main-template/main-template.component";
+
+@Component({
+  selector: 'app-main-page',
+  imports: [MainTemplateComponent],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+
+}
