539f1778d9ba8d8ca98428726e5db8eea7805df2
prchalPetr <peta.prchal@email.cz>
2026-01-02 16:28:47 +0100

    show simple main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from "./pages/main-page/main-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
