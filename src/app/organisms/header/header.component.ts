5425ba4341d363aaaf39dcbd1effb6b6158ca559
prchalPetr <peta.prchal@email.cz>
2026-01-02 16:26:56 +0100

    create basic header

---
+++ src/app/organisms/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Card } from "primeng/card";
+
+@Component({
+  selector: 'app-header',
+  imports: [Card],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
