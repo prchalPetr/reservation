019db3ad068c34a90b20e4b5a852a4baad5d1085
prchalPetr <peta.prchal@email.cz>
2026-01-02 16:27:15 +0100

    create basic footer

---
+++ src/app/organisms/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-footer',
+  imports: [CardModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
